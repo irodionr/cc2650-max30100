d43f791081fd793cbbb5c967a688656da959e480
Sergey <strelnikov-sv@yandex.ru>
2018-03-02 02:27:02 +0300

    Update HR

--- lib/MAX30100/MAX30100.cpp
+++ lib/MAX30100/MAX30100.cpp
@@ -1,6 +1,28 @@
 #include "MAX30100.h"
 #include "lib/MAXLIB/R_W.h"
 
+uint8_t returnFrom(uint8_t regAddr, uint8_t *txBuffer, uint8_t *rxBuffer, I2C_Transaction *i2cTransaction, I2C_Handle *i2c)
+{
+
+    txBuffer[0] = regAddr;
+    i2cTransaction->writeBuf = txBuffer;
+    i2cTransaction->writeCount = 1;
+    i2cTransaction->readBuf = rxBuffer;
+    i2cTransaction->readCount = 1;
+
+    if (I2C_transfer(*i2c, &(*i2cTransaction)))
+    {
+        return rxBuffer[0];
+    }
+    else
+    {
+        System_printf("I2C Bus Fault \n");
+    }
+	
+	System_flush();
+	return 0x0;
+}
+
 void taskFxn(UArg arg0, UArg arg1)
 {
     I2C_Handle      i2c;
@@ -8,34 +30,48 @@ void taskFxn(UArg arg0, UArg arg1)
     I2C_Transaction i2cTransaction;
     uint8_t         txBuffer[2];
     uint8_t         rxBuffer[1];
+	uint8_t			tmp;
+	uint16_t		dataIR;
+	uint16_t		dataR;
     i2cTransaction.slaveAddress = MAX30100_ADDRESS;
     i2cParams.bitRate = I2C_400kHz;
-
+	int i;
+	
     /* === I2C initialization === */
     I2C_Params_init(&i2cParams);
     i2c = I2C_open(CC2650STK_I2C0, &i2cParams);
     if (i2c == NULL){ System_printf("I2C initialization error\n"); }
     else { System_printf("\nI2C initialized\n\n"); }
 
-    /* === READ & WRITE === */
-    readFrom(MAX30100_PART_ID, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    System_printf("Part ID: 0x%x\n", rxBuffer[0]);
-    readFrom(MAX30100_REV_ID, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    System_printf("Rev ID: 0x%x\n", rxBuffer[0]);
-    writeTo(MAX30100_MODE_CONFIG, MAX30100_SPO2_ON, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    readFrom(MAX30100_MODE_CONFIG, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    if (rxBuffer[0]==0x2) { System_printf("HR only enabled\n"); }
-    if (rxBuffer[0]==0x3) { System_printf("SPO2 enabled\n"); }
-    writeTo(MAX30100_SPO2_CONFIG, pw200, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    readFrom(MAX30100_SPO2_CONFIG, txBuffer, rxBuffer, &i2cTransaction, &i2c);
-    if (rxBuffer[0]==0x0) { System_printf("Brightness level 1\n"); }
-    if (rxBuffer[0]==0x1) { System_printf("Brightness level 2\n"); }
-    if (rxBuffer[0]==0x2) { System_printf("Brightness level 3\n"); }
-    if (rxBuffer[0]==0x3) { System_printf("Brightness level 4\n"); }
-    writeTo(MAX30100_LED_CONFIG, 0xFF, txBuffer, rxBuffer, &i2cTransaction, &i2c);         //selected 50 mA & 50 mA
-    readFrom(MAX30100_LED_CONFIG, txBuffer, rxBuffer, &i2cTransaction, &i2c);              //ISSUES #1
-
-    /* === I2C Closing === */
+	/* === Measuring heart rate === */
+    writeTo(MAX30100_LED_CONFIG, 0xFF, txBuffer, rxBuffer, &i2cTransaction, &i2c); //LED current = 50 mA
+    writeTo(MAX30100_SPO2_CONFIG, 0x7, txBuffer, rxBuffer, &i2cTransaction, &i2c); //sample rate = 100 sps, LED pulse width = 1600 us, ADC resolution = 16 bits
+	writeTo(MAX30100_MODE_CONFIG, 0x2, txBuffer, rxBuffer, &i2cTransaction, &i2c); //mode = HR only
+	writeTo(MAX30100_INT_ENABLE, 0xA0, txBuffer, rxBuffer, &i2cTransaction, &i2c); //enable ALMOST_FULL and HR_READY interrupts
+	
+	writeTo(MAX30100_FIFO_WR_PTR, 0x0, txBuffer, rxBuffer, &i2cTransaction, &i2c); //clear write pointer
+	writeTo(MAX30100_FIFO_RD_PTR, 0x0, txBuffer, rxBuffer, &i2cTransaction, &i2c); //clear read pointer
+	
+	for (i = 0; i < 1; i++) { //number of samples to read
+		while (!tmp) { //waiting for HR_RDY flag in INTERRUPT_STATUS
+			tmp = returnFrom(MAX30100_INT_STATUS, txBuffer, rxBuffer, &i2cTransaction, &i2c);
+			tmp &= 0x20; //0x0010000 - HR_RDY flag
+			
+			//System_printf("Waiting for HR_RDY... \n");
+			//System_flush();
+		}
+		
+		dataIR = returnFrom(MAX30100_FIFO_DATA, txBuffer, rxBuffer, &i2cTransaction, &i2c); //1st byte - IR data for HR
+		dataIR = (dataIR << 8) + returnFrom(MAX30100_FIFO_DATA, txBuffer, rxBuffer, &i2cTransaction, &i2c); //2nd byte - IR data for HR
+		dataR = returnFrom(MAX30100_FIFO_DATA, txBuffer, rxBuffer, &i2cTransaction, &i2c); //3rd byte
+		dataR = (dataR << 8) + returnFrom(MAX30100_FIFO_DATA, txBuffer, rxBuffer, &i2cTransaction, &i2c); //4th byte
+		
+		System_printf("IR data: 0x%x\n", dataIR);
+		//System_printf("R data: 0x%x\n", dataR);
+		System_flush();
+	}
+	
+    /* === I2C closing === */
     I2C_close(i2c);
     System_printf("\nI2C Closed\n");
     System_flush();
